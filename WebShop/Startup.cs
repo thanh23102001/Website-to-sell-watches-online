75edf6ce0a96cc8644ad82d745984d5d8cf329ac
pham thanh <thanhpdhe150023@fpt.edu.vn>
2022-07-05 23:18:00 +0700

    Test Git Hub

--- WebShop/Startup.cs
+++ WebShop/Startup.cs
@@ -32,7 +32,7 @@ namespace WebShop
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Thanh!");
                 });
             });
         }
